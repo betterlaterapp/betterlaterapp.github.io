8d67ad9ea6e01abaa6b4435a676ce17a2cfbe9d3
Corey Boiko <ididntrealize@gmail.com>
2024-06-26 23:11:41 -0800

    fixed service worker

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -1,11 +1,12 @@
 var version = "v3.14::pages";
-
-self.addEventListener("activate", (event) => {
+self.addEventListener('install', function(event){});
+self.addEventListener('activate', event => {
   const cacheWhitelist = [version];
+
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map((cacheName) => {
+        cacheNames.map(cacheName => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
@@ -15,36 +16,43 @@ self.addEventListener("activate", (event) => {
   );
 });
 
-self.addEventListener("fetch", function (event) {
-  if (event.request.method !== "GET") {
+self.addEventListener("fetch", function(event) {
+  console.log('WORKER: fetch event in progress.');
+  if (event.request.method !== 'GET') {
     return;
   }
   event.respondWith(
-    caches.match(event.request).then(function (cached) {
-      var networked = fetch(event.request)
-        .then(fetchedFromNetwork, unableToResolve)
-        .catch(unableToResolve);
-      return cached || networked;
+    caches
+      .match(event.request)
+      .then(function(cached) {
+        var networked = fetch(event.request)
+          .then(fetchedFromNetwork, unableToResolve)
+          .catch(unableToResolve);
+        
+          return cached || networked;
 
-      function fetchedFromNetwork(response) {
-        var cacheCopy = response.clone();
-        caches.open(version).then(function add(cache) {
-          if(!event.request.url.startsWith('http')){
-            cache.put(event.request, cacheCopy);
-          }
-        });
-        return response;
-      }
+        function fetchedFromNetwork(response) {
+          var cacheCopy = response.clone();
+          caches.open(version)
+            .then(function add(cache) {
+              cache.put(event.request, cacheCopy);
+            })
+            .then(function() {
+              console.log('WORKER: fetch response stored in cache.', event.request.url);
+            });
 
-      function unableToResolve() {
-        return new Response("<h1>Service Unavailable</h1>", {
-          status: 503,
-          statusText: "Service Unavailable",
-          headers: new Headers({
-            "Content-Type": "text/html",
-          }),
-        });
-      }
-    })
+          return response;
+        }
+
+        function unableToResolve () {
+          return new Response('<h1>Service Unavailable</h1>', {
+            status: 503,
+            statusText: 'Service Unavailable',
+            headers: new Headers({
+              'Content-Type': 'text/html'
+            })
+          });
+        }
+      })
   );
 });
