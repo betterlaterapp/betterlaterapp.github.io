9c4b3b1e5ddacd2d831cb2ce52c46a048779605f
Corey Boiko <ididntrealize@gmail.com>
2025-11-16 17:34:00 -0500

    app version reflect ai introduction

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -1,4 +1,4 @@
-var version = "v3.16::pages";
+var version = "v3.14.1::pages";
 
 self.addEventListener('install', function(event) {
   // No need for empty function, but we can use it for precaching if needed
