6bca5bcfd7dab7f85a5a411130036b0fe5fe37e0
Corey Boiko <ididntrealize@gmail.com>
2025-12-11 01:34:54 -0500

    IOS safari input styling, partial infrastructure for versioning before git push hook

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -1,4 +1,4 @@
-var version = "v3.14.10::pages";
+var version = "v3.14.11::pages";
 
 self.addEventListener('install', function(event) {
   // No need for empty function, but we can use it for precaching if needed
