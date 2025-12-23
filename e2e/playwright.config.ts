88566681ffd6cb354ffcc11b115d37c00e30a219
Corey Boiko <ididntrealize@gmail.com>
2025-12-22 22:49:56 -0500

    organize playwright related code and cofing

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -5,7 +5,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './e2e',
+  testDir: './tests',
   
   // Run tests in parallel for faster execution
   fullyParallel: true,
